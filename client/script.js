c0494da2712b95c17acf6e565919ec9c673e1b78
Got-crypto <pecpalmplay@gmail.com>
2023-01-04 02:09:36 +0200

    changed url to be fetched

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ async function handleSubmit(e){
 
 
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://afk-codex.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
